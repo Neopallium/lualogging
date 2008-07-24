c9a1ee946a12d73b9eff41a57c9ea9764f120c42
Alessandro Hecht <alessandrohc@luaforge.net>
2008-07-24 20:17:25 +0000

    new test basic

--- tests/testFile.lua
+++ tests/testFile.lua
@@ -5,5 +5,6 @@ local logger = logging.file("test%s.log", "%Y-%m-%d")
 logger:info("logging.file test")
 logger:debug("debugging...")
 logger:error("error!")
+logger:info({id = "1"})
 
 print("File Logging OK")
