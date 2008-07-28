4dfdf0127057bfe108a0204c04ccbe38ebf07d4b
Alessandro Hecht <alessandrohc@luaforge.net>
2008-07-28 16:23:38 +0000

    Fix the problem cause when the logger in file use the datePattern, but not considered the change of the date.

--- src/logging/file.lua
+++ src/logging/file.lua
@@ -5,24 +5,51 @@
 --
 -- @copyright 2004-2007 Kepler Project
 --
--- @release $Id: file.lua,v 1.5 2007-09-05 12:15:31 tomas Exp $
+-- @release $Id: file.lua,v 1.6 2008-07-28 16:23:38 alessandrohc Exp $
 -------------------------------------------------------------------------------
 
 require"logging"
+
+local lastFileNameDatePattern
+local lastFileHandler
+
+local openFileLogger = function (filename, datePattern)
+	
+	local filename = string.format(filename, os.date(datePattern))
+	if (lastFileNameDatePattern ~= filename) then
+		
+		local f = io.open(filename, "a")
+		if (f) then
+			f:setvbuf ("line")
+			lastFileNameDatePattern = filename
+			lastFileHandler = f
+			return f
+		else
+			return nil, string.format("file `%s' could not be opened for writing", filename)	
+		end
+	else
+		return lastFileHandler		
+	end
+	
+end
 
 function logging.file(filename, datePattern, logPattern)
 
     if type(filename) ~= "string" then
         filename = "lualogging.log"
     end
-    filename = string.format(filename, os.date(datePattern))
-    local f = io.open(filename, "a")
+    
+    local f, msg = openFileLogger(filename, datePattern)
     if not f then
-       return nil, string.format("file `%s' could not be opened for writing", filename)
-    end
-    f:setvbuf ("line")
+       return nil, msg
+    end    
 
     return logging.new( function(self, level, message)
+                            
+                            local f, msg = openFileLogger(filename, datePattern)
+                            if not f then
+						       return nil, msg
+						    end              
                             local s = logging.prepareLogMsg(logPattern, os.date(), level, message)
                             f:write(s)
                             return true
