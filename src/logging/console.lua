f5b02df67fdaaa81f926a358381d75be71fe30a7
Thijs Schreijer <thijs@thijsschreijer.nl>
2012-09-19 22:47:43 +0200

    updated 2011 to 2012 for copyrights

--- src/logging/console.lua
+++ src/logging/console.lua
@@ -3,7 +3,7 @@
 --
 -- @author Thiago Costa Ponte (thiago@ideais.com.br)
 --
--- @copyright 2004-2011 Kepler Project
+-- @copyright 2004-2012 Kepler Project
 --
 -------------------------------------------------------------------------------
 
