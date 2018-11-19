94d67c173023afc2049b821561058a84973cb847
Dante Garin <dante.garin@axiovista.com>
2018-11-19 16:32:44 -0300

    Support for logrotate, when file has been deleted

--- src/logging/file.lua
+++ src/logging/file.lua
@@ -1,7 +1,7 @@
 -------------------------------------------------------------------------------
 -- Saves logging information in a file
 --
--- @author Thiago Costa Ponte (thiago@idaeais.com.br)
+-- @author Thiago Costa Ponte (thiago@ideais.com.br)
 --
 -- @copyright 2004-2013 Kepler Project
 --
@@ -9,46 +9,36 @@
 local logging = require"logging"
 logging_fileHandler = {} -- store the file handlers
 
-function exists(fn)
-        local f = io.open(fn,"r")
-        if f ~= nil then
-                io.close(f)
-                return true
-        else
-                return false
-        end
-end
-
 function openFileLogger(filename, datePattern)
-        local filename = string.format(filename, os.date(datePattern))
-        if logging_fileHandler[filename] == nil or not exists(filename) then
-                local f = io.open(filename, "a")
-                if (f) then
-                        f:setvbuf ("line")
-                        logging_fileHandler[filename] = f
-                        return f
-                else
-                        return nil, string.format("file `%s' could not be opened for writing", filename)
-                end
-        else
-                return logging_fileHandler[filename]
-        end
+	local filename = string.format(filename, os.date(datePattern))
+	if logging_fileHandler[filename] == nil then
+		f = io.open(filename, "a")
+		if (f) then
+			f:setvbuf ("line")
+			logging_fileHandler[filename] = f
+			return f
+		else
+			return nil, string.format("file `%s' could not be opened for writing", filename)
+		end
+	else
+		return logging_fileHandler[filename]
+	end
 end
 
 function logging.file(filename, datePattern, logPattern)
-        if type(filename) ~= "string" then
-                filename = "lualogging.log"
-        end
+	if type(filename) ~= "string" then
+		filename = "lualogging.log"
+	end
 
-        return logging.new( function(self, level, message)
-                local f, msg = openFileLogger(filename, datePattern)
-                if not f then
-                        return nil, msg
-                end
-                local s = logging.prepareLogMsg(logPattern, os.date(), level, message)
-                f:write(s)
-                return true
-        end)
+	return logging.new( function(self, level, message)
+		local f, msg = openFileLogger(filename, datePattern)
+		if not f then
+			return nil, msg
+		end
+		local s = logging.prepareLogMsg(logPattern, os.date(), level, message)
+		f:write(s)
+		return true
+	end)
 end
 
 return logging.file
